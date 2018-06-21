155461ca881b14f42914d558e3b91133eabc9a22
Kyleigh Knight <kyleigh.knight@hotmail.com>
2018-06-20 22:22:28 -0500

    Update convert-csv-to-json

---
+++ parse-csv.ts
@@ -0,0 +1,23 @@
+﻿/** convert-csv-to-json === csv to json parser (https://www.npmjs.com/package/convert-csv-to-json)*/
+
+import * as tapTypes from './tap-types'
+var csv = require('convert-csv-to-json')
+var fs = require('fs')
+
+let csvFile = '../testdata/email/test.csv'
+let jsonOutput = '../testdata/email/output.json'
+
+export function parseItem(test: Buffer) {
+  return new Promise(function(resolve, reject) {
+    csv(test, function(err: any, result: any) {
+      if (result != null) {
+        csv.fieldDelimiter(',').getJsonFromCsv(csvFile)
+        csv.generateJsonFileFromCsv(csvFile, jsonOutput)
+      } else {
+        reject(Error('Danger Will Robertson! Something went wrong.'))
+      }
+    })
+  })
+}
+
+/* drop in promise */
---
+++ scan-dir.ts
@@ -0,0 +1,72 @@
+/**
+<<<<<<< HEAD
+ * A "scanner" scans a resource collection, parsing the items it finds using the parser passed in  (see the [spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md))
+=======
+ * A "scanner" scans a resource collection, parsing the items it finds using the parser passed in
+ * (see the [spec](https://github.com/singer-io/getting-started/blob/master/docs/SPEC.md#singer-specification))
+>>>>>>> 3cb98bb97ac99d0e2d2a299c63c32e788f5f29d6
+ *
+ * In this case, we are scanning a directory and parsing the files inside.
+ */ /** this is a dummy single-line comment needed for documentation build; a hack for https://github.com/TypeStrong/typedoc/issues/603 */
+
+/** fs-extra is a promise-enabled superset of the standard fs package */
+import * as fse from 'fs-extra'
+import * as tapTypes from './tap-types'
+var csv = require('convert-csv-to-json')
+var fs = require('fs')
+
+/** generate json-schemas for our records, if needed */
+var generateSchema = require('generate-schema') // typescript types aren't available so we load javascript-style instead of using typescript's import
+
+/**
+ * Scan a folder, running parser on each file it finds
+ * - TODO: implement configObjs.state and configObjs.catalog, which are just stubs for now
+ * - TODO: use interfaces instead of "any" here
+ * @param configObjs
+ * @param parser
+ */
+export async function scanDir(configObjs: any, parser: any) {
+  let config = configObjs.config
+  // future config options
+  let state = configObjs.state
+  let catalog = configObjs.catalog
+
+  // TODO: allow schema(s) to be passed in in config
+  let schema: any = null
+
+//<<<<<<< HEAD
+  return fse
+    .readdir(config.target_folder)
+    .then(function(filelist) {
+      return Promise.all(
+        // return an array of promises, one per filename, for Promise.all to run asynchronously
+        filelist.map(function(filename, idx) {
+          return fse.readFile(config.target_folder + '/' + filelist[idx]).then(function(buffer) {
+            // the parsing is done here
+            return parser(buffer)
+          })
+        })
+      )
+    })
+//=======
+.then(function(parsedObjs) {
+  if (parsedObjs.length == 0) return null
+
+  let schm = new tapTypes.streamSchema()
+
+  // if no schema exists, create a schema based on the first new object
+  if (!schm.schema) schm.schema = generateSchema.json(parsedObjs[0].record)
+  schm.stream = parsedObjs[0].stream
+
+  // write the schema
+  console.log(JSON.stringify(schm))
+
+  // write the objects
+  parsedObjs.forEach(function(parsedObj, idx) {
+    console.log(JSON.stringify(parsedObj))
+  })
+
+  // TODO: write STATE record
+  return parsedObjs
+})
+}
